19e9e22c6e0398e61484e527a14cd1d5fe9d8568
Henju <luhenpinho@gmail.com>
2019-12-06 20:45:19 -0300

    Just updating

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,87 @@
 import React from 'react';
+import './Main.css';
+
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
 
 export default function Main() {
-    return <h1>Hello World</h1>
+    return (
+        <div className="main-container">
+            <img src={logo} alt="Tindev" />
+            <ul>
+                <li>
+                    <img src="https://i.pinimg.com/originals/6b/0a/ae/6b0aae375a530c8f0de1327bae2eef55.jpg" alt="" />
+                    <footer>
+                        <strong>Yasuo True Damage</strong>
+                        <p>Destruidor da SoloQ</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+
+                        <button type="button">
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://i.pinimg.com/originals/6b/0a/ae/6b0aae375a530c8f0de1327bae2eef55.jpg" alt="" />
+                    <footer>
+                        <strong>Yasuo True Damage</strong>
+                        <p>Destruidor da SoloQ</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+
+                        <button type="button">
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://i.pinimg.com/originals/6b/0a/ae/6b0aae375a530c8f0de1327bae2eef55.jpg" alt="" />
+                    <footer>
+                        <strong>Yasuo True Damage</strong>
+                        <p>Destruidor da SoloQ</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+
+                        <button type="button">
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://i.pinimg.com/originals/6b/0a/ae/6b0aae375a530c8f0de1327bae2eef55.jpg" alt="" />
+                    <footer>
+                        <strong>Yasuo True Damage</strong>
+                        <p>Destruidor da SoloQ</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike" />
+                        </button>
+
+                        <button type="button">
+                            <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
 }
